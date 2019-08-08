17e5eeb152c230cc69db6ac898aa3426d42d0c3d
Antti Nieminen <antti.nieminen@ericsson.com>
2019-08-08 13:14:58 +0300

    corrected DOM elements

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({ handleClick, text }) => (
-        <button onClick={handleClick}>{text}</button>
+    <button onClick={handleClick}>{text}</button>
 )
 
-const DisplayMostVotes = ({points}) => {
+const DisplayMostVotes = ({ points }) => {
     console.log("points: ", points)
     const copy = [...points]
     var mostVotesIndex = 0;
-    for(var i = 0; i < copy.length; i++) {
+    for (var i = 0; i < copy.length; i++) {
         if (points[i] > points[mostVotesIndex]) {
             mostVotesIndex = i
         }
@@ -18,18 +18,16 @@ const DisplayMostVotes = ({points}) => {
 
     return (
         <>
-        <h3>Anecdote with most votes</h3>
-        <p>
-            {anecdotes[mostVotesIndex]}
+            <h3>Anecdote with most votes</h3>
+            <p>{anecdotes[mostVotesIndex]}</p>
             <p>has {points[mostVotesIndex]} votes</p>
-        </p>
         </>
     )
 }
 
 
 const App = (props) => {
-    
+
     const [points, setPoints] = useState(props.points)
     const [selected, setSelected] = useState(0)
 
@@ -50,7 +48,7 @@ const App = (props) => {
             <p>has {points[selected]} votes</p>
             <Button handleClick={() => giveRandomAnecdote()} text="next anecdote" />
             <Button handleClick={() => addVote()} text="vote" />
-            <DisplayMostVotes points={points}/>
+            <DisplayMostVotes points={points} />
         </div>
     )
 }
@@ -73,6 +71,6 @@ function randomNumber(max) {
 }
 
 ReactDOM.render(
-    <App anecdotes={anecdotes} points={points}/>,
+    <App anecdotes={anecdotes} points={points} />,
     document.getElementById('root')
 )
