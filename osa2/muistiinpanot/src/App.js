00509ae4c31aa53919f2d53ccc407d283c54a517
Antti Nieminen <antti.nieminen@ericsson.com>
2019-08-13 13:15:05 +0300

    muistiinpanot osa 2e

--- osa2/muistiinpanot/src/App.js
+++ osa2/muistiinpanot/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
 
@@ -8,6 +7,19 @@ const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState()
+
+    const Notification = ({ message }) => {
+        if (message === null) {
+          return null
+        }
+      
+        return (
+          <div className="error">
+            {message}
+          </div>
+        )
+      }
 
     useEffect(() => {
         noteService
@@ -47,9 +59,12 @@ const App = () => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
-                alert(
-                  `the note '${note.content}' was already deleted from server`
-                )
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
+                  )
+                  setTimeout(() => {
+                    setErrorMessage(null)
+                  }, 5000)
                 setNotes(notes.filter(n => n.id !== id))
               })
     }
@@ -71,6 +86,9 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+
+            <Notification message={errorMessage} />
+
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
